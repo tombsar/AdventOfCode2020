f69e385045e433eef6c3428490c6b54e6795b74a
A. Tombs <arthur.tombs@gmail.com>
2020-12-01 19:19:54 +0000

    Solutions to Day 1

---
+++ day01b.c
@@ -0,0 +1,30 @@
+#include "common.h"
+#include "vector.h"
+
+int main (int argc, char ** argv) {
+    Vector_t v;
+    vector_init(&v, 256);
+    do {
+        long in;
+        int ret = scanf("%li", &in);
+        if (ret == 1) {
+            vector_push_back(&v, in);
+        } else if (ret == EOF) {
+            break;
+        } else {
+            ERROR("scanf failed");
+        }
+    } while (1);
+
+    for (size_t i = 0; i < (v.count-3); ++i) {
+        for (size_t j = i+1; j < (v.count-2); ++j) {
+            long target = 2020 - v.data[i] - v.data[j];
+            for (size_t k = j+1; k < v.count; ++k) {
+                if (v.data[k] == target) {
+                    printf("Answer: %li\n", v.data[i]*v.data[j]*v.data[k]);
+                    exit(EXIT_SUCCESS);
+                }
+            }
+        }
+    }
+}
