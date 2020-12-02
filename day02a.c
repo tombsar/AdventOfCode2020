8f798f480d1eb5f3cb567b958012b411a2fc5168
A. Tombs <arthur.tombs@gmail.com>
2020-12-02 05:25:41 +0000

    Add solutions for Day 2

---
+++ day02a.c
@@ -0,0 +1,40 @@
+#include "common.h"
+
+typedef struct Policy {
+    int n_min;
+    int n_max;
+    char c;
+} Policy_t;
+
+_Bool validate (char const * pass, Policy_t policy) {
+    int n_c = 0;
+    char const * p = pass;
+    while (*p != '\0') {
+        if (*p == policy.c) {
+            ++n_c;
+        }
+        ++p;
+    }
+    return (n_c >= policy.n_min && n_c <= policy.n_max);
+}
+
+int main (int argc, char ** argv) {
+    int n_valid = 0;
+    do {
+        Policy_t policy;
+        // HACK: Potential buffer overflow!
+        char password [256];
+        int ret = scanf("%i-%i %c: %s", &(policy.n_min), &(policy.n_max), &(policy.c), &(password[0]));
+        if (ret == 4) {
+            if (validate(password, policy)) {
+                ++n_valid;
+            }
+        } else if (ret == EOF) {
+            break;
+        } else {
+            ERROR("scanf failed");
+        }
+    } while (1);
+
+    DISP(n_valid, "%i");
+}
---
+++ day02b.c
@@ -0,0 +1,39 @@
+#include "common.h"
+
+typedef struct Policy {
+    int pos0;
+    int pos1;
+    char c;
+} Policy_t;
+
+_Bool validate (char const * pass, Policy_t policy) {
+    int n_c = 0;
+    if (pass[policy.pos0-1] == policy.c) {
+        ++n_c;
+    }
+    if (pass[policy.pos1-1] == policy.c) {
+        ++n_c;
+    }
+    return (n_c == 1);
+}
+
+int main (int argc, char ** argv) {
+    int n_valid = 0;
+    do {
+        Policy_t policy;
+        // HACK: Potential buffer overflow!
+        char password [256];
+        int ret = scanf("%i-%i %c: %s", &(policy.pos0), &(policy.pos1), &(policy.c), &(password[0]));
+        if (ret == 4) {
+            if (validate(password, policy)) {
+                ++n_valid;
+            }
+        } else if (ret == EOF) {
+            break;
+        } else {
+            ERROR("scanf failed");
+        }
+    } while (1);
+
+    DISP(n_valid, "%i");
+}
